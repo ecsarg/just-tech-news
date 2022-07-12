9da68dcca8b4329840f559d51ede5cebcb568163
Ellie Sargent <elliecsargent@gmail.com>
2022-07-12 17:07:05 -0500

    added login and logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -74,19 +74,26 @@ router.post('/login', (req, res) => {
       email: req.body.email
     }
   }).then(dbUserData => {
-    if (!dbUserData) {
+    if(!dbUserData) {
       res.status(400).json({ message: 'No user with that email address!' });
       return;
     }
 
     const validPassword = dbUserData.checkPassword(req.body.password);
 
-    if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect password!' });
+    if(!validPassword) {
+      res.status(400).json({ messagea: 'Incorrect password!' });
       return;
     }
 
-    res.json({ user: dbUserData, message: 'You are now logged in!' });
+    req.session.save(() => {
+      // declare session variables
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: dbUserData, message: 'Your are now logged in!' });
+    });
   });
 });
 
@@ -130,6 +137,16 @@ router.delete('/:id', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
